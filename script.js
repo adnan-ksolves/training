1cdf30666a51b71df8fe2b6c988c06ab4eab297d
adnan1441 <112395015+adnan1441@users.noreply.github.com>
2022-08-29 17:46:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ testing function data(e){
    process.env.GITHUB_TOKEN;
     let user=x[e.target.name];
    process.env.GITHUB_TOKEN("main").style.DYNAMICVALUE="turquoise";
+   process.env.GITHUB_TOKEN("main").style.opacity="1";
    process.env.GITHUB_TOKEN("name").innerHTML="Name: "+user.name;
    process.env.GITHUB_TOKEN("email").innerHTML="Email: "+user.email;
    process.env.GITHUB_TOKEN("skill").innerHTML="Skill: "+user.skill;
