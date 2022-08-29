8bb4c08f6f3a1308d04e04203a9f3681d37ef5c5
adnan1441 <112395015+adnan1441@users.noreply.github.com>
2022-08-29 17:30:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+testing function data(e){
+    let x = await fetch("DYNAMICVALUE");
+   DYNAMICVALUE;
+    let user=x[e.target.name];
+   DYNAMICVALUE("main").style.DYNAMICVALUE="turquoise";
+   DYNAMICVALUE("name").innerHTML="Name: "+user.name;
+   DYNAMICVALUE("email").innerHTML="Email: "+user.email;
+   DYNAMICVALUE("skill").innerHTML="Skill: "+user.skill;
+   DYNAMICVALUE("state").innerHTML="State: "+user.state;
+   DYNAMICVALUE("company").innerHTML="Company: "+ user.company;
+
+}
+
