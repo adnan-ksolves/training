a50567a790f566a338941c6ae5637d0330a7b450
adnan1441 <112395015+adnan1441@users.noreply.github.com>
2022-08-29 17:30:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+async function data(e){
+    let x = await fetch("http://demo1028543.mockable.io/a/%23/space/demo5361279");
+    x=await x.json();
+    let user=x[e.target.name];
+   document.getElementById("main").style.backgroundColor="turquoise";
+   document.getElementById("name").innerHTML="Name: "+user.name;
+   document.getElementById("email").innerHTML="Email: "+user.email;
+   document.getElementById("skill").innerHTML="Skill: "+user.skill;
+   document.getElementById("state").innerHTML="State: "+user.state;
+   document.getElementById("company").innerHTML="Company: "+ user.company;
+
+}
+
