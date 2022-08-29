cc9003229a4b7eda32f5aba5e9451d92368a547f
adnan1441 <112395015+adnan1441@users.noreply.github.com>
2022-08-29 17:46:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ testing function data(e){
    DYNAMICVALUE;
     let user=x[e.target.name];
    DYNAMICVALUE("main").style.DYNAMICVALUE="turquoise";
+   DYNAMICVALUE("main").style.opacity="1";
    DYNAMICVALUE("name").innerHTML="Name: "+user.name;
    DYNAMICVALUE("email").innerHTML="Email: "+user.email;
    DYNAMICVALUE("skill").innerHTML="Skill: "+user.skill;
