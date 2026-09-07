a68075379484ed0048adc8a28bb12f20edafcf21
Adnan Ur Rehman <adnan.rehman@ksolves.com>
2026-09-07 14:04:36 +0530

    testing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 let data=testing(e)=>{
-    let x = await fetch("process.env.GITHUB_TOKEN");
+    let x = await fetch("test");
    process.env.GITHUB_TOKEN;
     let user=x[e.target.name];
    process.env.GITHUB_TOKEN("main").style.DYNAMICVALUE="turquoise";
